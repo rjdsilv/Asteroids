c7a0b091ad7e9dc95e3e2c108a73ea35354933b0
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2017-11-25 18:02:44 -0500

    Created the ship movement.

---
+++ Assets/Scripts/EuclideanTorus.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class EuclideanTorus : MonoBehaviour {
+    /// Update is called once per frame
+    /// This method will updated the ship position after it left the screen.
+    /// If the ship left the screen on up, it will appear down.
+    /// If the ship left the screen on down, it will appear up.
+    /// If the ship left the screen on right, it will appear left.
+    /// If the ship left the screen on left, it will appear right.
+    void Update ()
+    {
+        // Setting up the x axis.
+		if(transform.position.x > 9)
+        {
+            transform.position = new Vector3(-9, transform.position.y, 0);
+        }
+        if (transform.position.x < -9)
+        {
+            transform.position = new Vector3(9, transform.position.y, 0);
+        }
+
+        // Setting up the y axis.
+        if (transform.position.y > 6)
+        {
+            transform.position = new Vector3(transform.position.x, -6, 0);
+        }
+        if (transform.position.y < -6)
+        {
+            transform.position = new Vector3(transform.position.x, 6, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+    const float MAX_SPEED = 10f;
+    const float MIN_SPEED = 0f;
+
+    private Rigidbody2D shipRigidBody2D;
+    private Animator shipAnimator;
+
+    /// <summary>
+    /// Starts the ship game processament.
+    /// </summary>
+    private void Start()
+    {
+        shipRigidBody2D = GetComponent<Rigidbody2D>();
+        shipAnimator = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// This method is called once per frame for physics. It will make the ship move through the screen.
+    /// </summary>
+    void FixedUpdate ()
+    {
+        float acceleration = Input.GetAxis("Accelerate");
+        float rotation = Input.GetAxis("Rotate");
+
+        // Accelerate the ship ntil a maximum speed.
+        if (acceleration > 0)
+        {
+            shipRigidBody2D.AddForce(transform.up * 3f * acceleration);
+            if (shipRigidBody2D.velocity.magnitude > MAX_SPEED)
+            {
+                shipRigidBody2D.velocity = shipRigidBody2D.velocity.normalized * MAX_SPEED;
+            }
+        }
+
+        // Rotate the ship in the z axis.
+        transform.Rotate(0, 0, -rotation * 5f);
+	}
+}
